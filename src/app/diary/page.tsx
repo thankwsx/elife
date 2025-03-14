cebe3005d8ff3463345ccde03de8caaf65748ff2
jackyqi <thankwsx@gmail.com>
2025-03-14 23:00:46 +0800

    feat: 更新日记UI

--- src/app/diary/page.tsx
+++ src/app/diary/page.tsx
@@ -1,22 +1,33 @@
 type DiaryEntity = {
-    id: string;
-    title: string;
-    content: string;
+  id: string;
+  title: string;
+  content: string;
 };
 
 export default async function DiaryList() {
-  const resp = await fetch("https://strapi.jackyqi.cn/api/diarys");
+  const resp = await fetch(
+    "https://strapi.jackyqi.cn/api/diarys?sort=title:desc"
+  );
   const diaryList = await resp.json();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {diaryList.data.map((diary: DiaryEntity) => {
-        return (
-          <div key={diary.id} className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-full">
-            <h2 className="text-gray-900">{diary.title}</h2>
-            <p className="text-sm/6 text-gray-700"><pre>{diary.content}</pre></p>
-          </div>
-        );
-      })}
+    <div className="min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="grid grid-cols-1 gap-8">
+        {diaryList.data?.map((diary: DiaryEntity) => {
+          return (
+            <div
+              key={diary.id}
+              className="w-full flex flex-col gap-4 p-4 bg-white rounded-lg shadow-md"
+            >
+              <h2 className="text-lg font-semibold text-gray-900">
+                {diary.title}
+              </h2>
+              <p className="text-sm text-gray-700 whitespace-pre-wrap">
+                {diary.content}
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
